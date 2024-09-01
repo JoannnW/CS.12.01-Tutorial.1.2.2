5eda9877bdba6745dff1a3eb3989949c07e148b0
JoannnW <143521580+JoannnW@users.noreply.github.com>
2024-09-01 10:10:57 +0800

    Done

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -14,11 +14,11 @@ public class HiddenWord {
     public String getHint(String guess){
         String str="";
             if (!guess.toUpperCase().equals(guess)){
-                throw new IllegalArgumentException("Your guess ("+guess+") does not contain uppercase letters. THe guess must be in uppercase!");
+                throw new IllegalArgumentException("Your guess ( "+guess+" ) does not contain all uppercase letters. Your guess must contain all uppercase letters!");
                 // throwing this instead of returning a String explicitly shows that an error has occurred
             }
             if (guess.length()!=hiddenWord.length()){
-                throw new IllegalArgumentException("Your guess ("+guess+") has " + guess.length()+" characters. The hidden word has "+hiddenWord.length()+" characters. Number of characters must match!");
+                throw new IllegalArgumentException("Your guess ( "+guess+" ) has "+guess.length()+" characters. The hidden word has "+hiddenWord.length()+" characters. Your guess must be a word with "+hiddenWord.length()+" characters!");
             }
         for(int i=0; i<guess.length(); i++){
             if (guess.charAt(i) == (hiddenWord.charAt(i))){
